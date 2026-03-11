de0352ec045ca12640d787567e37a7abe7858a20
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-03-11 13:10:24 +0600

    a

---
+++ ChallengeSolution/quiz_6.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int favorite;
+cout << "Enter your favorite number: ";
+cin >> favorite;
+if (favorite == 13)
+   cout << "That my favorite number too!" << endl;
+   cout << "That's amazing!" << endl;
+   cout << "Great minds think alike!" << endl;
+
+    cout << endl;
+    return 0;
+}
+
+
---
+++ Quiz6/questio4.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Question 1
+    // What is the value of num after the following code executes
+    // if the user enters "10" at they key board?
+    // int num;
+    // cin >> num;
+
+    // if (num > 10)
+    //     num -= 10;
+    // else
+    //     num += 10;
+    // Ans: 20
+
+     // Question 2
+     // What is the following code snippet display if the user enters
+     // "70" at the keyboard?
+    //  int temperature;
+    //  cout << "Enter a temperature: ";
+    //  cin >> temperature;
+     
+    //  if (temperature < 50);
+    //     cout << "It's cold!" << endl;
+
+    // if (temperature > 50);
+    //     cout << "It's hot!" << endl;
+    
+    // else
+    //     cout "Maybe it's raining?";
+    
+
+     // Question 2
+     // What does the following code snippet display if the user 
+     // enters "20" at the keyboard?
+     int favorite;
+     cout << "Enter your favorite number: ";
+     cin >> favorite;
+     if (favorite == 13)
+        cout << "That my favorite number too!" << endl;
+        cout << "That's amasing!" << endl;
+        cout << "Great minds think alike!" << endl;
+
+        
+
+
+ 
+    // Question 4
+    // int num = 10;
+    // while (num >= 1)
+    //     cout << num << " ";
+    // num --;
+
+
+    // Question 5
+    // The while loop is an example of a(n) --> pre-test loop
+
+    // Question 6
+    // A do-while loop is guaranteed to execute --> at least one time
+
+    // Question 7
+    // The for loop has 3 expressions in the following order :
+    // initialize, test, increment
+
+    // Question 8
+    // A loop that is located inside another loop is called a(n) --> nested loop
+
+    // Question 9
+    // In order to terminate the execution of a loop, 
+    // we can use the .. brake statement
+
+    // Question 10
+    // If you know ahead of time how many times yo need to loop,
+    // which loop would you use?
+    // Ans:  for loop
+
+    cout << endl;
+    return 0;
+}
+
+
