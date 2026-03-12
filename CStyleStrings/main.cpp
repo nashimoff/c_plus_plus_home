65181bfd0b7f203c8c9b31431ecd0819d669924e
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-03-12 17:09:53 +0600

    o

---
+++ CStyleStrings/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring> // for c-style string functions
+#include <cctype> // for character-based functions
+
+using namespace std;
+
+int main() 
+{
+	char first_name[20] {};
+	char last_name[20] {};
+	char full_name[50] {};
+	char temp[50] {};
+
+	// cout << first_name; // Will likely display garbage!
+
+	cout << "Pleas enter your first name: ";
+	cin >> first_name;
+
+	cout << "Please enter your last name: ";
+	cin << last_name;
+	cout << "-------------------------" << endl;
+
+	cout <<"Hello, " << first_name << " your first name has " << strlen(first_name) << " characters" << endl;
+	cout <<"and your last name, " << last_name << " has " << strlen(last_name) << " characters" << endl;
+
+	strcpy(full_name, first_name);  // copy first_name to full_name
+	strcat(full_name, " ");         // concatenate full_name and a space
+	strcat(full_name, last_name);   // concatenate last_name to full_name
+	
+	// cout << "Default Template" << endl;
+	return 0;
+}
---
+++ CStyleStrings/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+cout << first_name; // Will likely display garbage!
+
+	// cout << "Pleas enter your first name: ";
+	// cin >> first_name;
+
+	// cout << "Please enter your last name: ";
+	// cin << last_name;
+	// cout << "-------------------------" << endl;
