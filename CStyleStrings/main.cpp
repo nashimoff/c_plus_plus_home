89eb013a7c4d9d180d4191a47de0230825f746a9
Nurlan Ashimov <nashimoff@gmail.com>
2026-03-14 14:42:21 +0600

    a

--- CStyleStrings/main.cpp
+++ CStyleStrings/main.cpp
@@ -13,19 +13,43 @@ int main()
 
 	// cout << first_name; // Will likely display garbage!
 
-	cout << "Pleas enter your first name: ";
-	cin >> first_name;
+	// cout << "Pleas enter your first name: ";
+	// cin >> first_name;
 
-	cout << "Please enter your last name: ";
-	cin << last_name;
-	cout << "-------------------------" << endl;
+	// cout << "Please enter your last name: ";
+	// cin >> last_name;
+	// cout << "-------------------------" << endl;
 
-	cout <<"Hello, " << first_name << " your first name has " << strlen(first_name) << " characters" << endl;
-	cout <<"and your last name, " << last_name << " has " << strlen(last_name) << " characters" << endl;
+	// cout <<"Hello, " << first_name << " your first name has " << strlen(first_name) << " characters" << endl;
+	// cout <<"and your last name, " << last_name << " has " << strlen(last_name) << " characters" << endl;
 
-	strcpy(full_name, first_name);  // copy first_name to full_name
-	strcat(full_name, " ");         // concatenate full_name and a space
-	strcat(full_name, last_name);   // concatenate last_name to full_name
+	// strcpy(full_name, first_name);  // copy first_name to full_name
+	// strcat(full_name, " ");         // concatenate full_name and a space
+	// strcat(full_name, last_name);   // concatenate last_name to full_name
+	// cout << "Your full name is " << full_name << endl;
+
+	// cout << "------------------------------------------------" << endl;
+	// cout << "Enter your full name: ";
+	// cin  >> full_name;
+
+	// cout << "Your full name is " << full_name << endl;
+
+	cout << "Enter your full name: ";
+	cin.getline(full_name, 50);
+	cout << "Your full name is " << full_name << endl;
+
+	cout << "------------------------------------------------" << endl;
+	strcpy(temp, full_name);
+	if(strcmp(temp, full_name) == 0)
+		cout << temp << " and " << full_name << " are the same" << endl;
+	else
+		cout << temp << " and " << full_name << " are different" << endl;
+	cout << "------------------------------------------------" << endl;
+
+	// for(size_t i{0}; i < strlen(full_name); ++i){
+	// 	if(isalpha(full_name[i]))
+	// 		full_name[i] == toupper(full_name[i]);
+	// }
 	
 	// cout << "Default Template" << endl;
 	return 0;
